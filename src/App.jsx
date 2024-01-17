82e0c292866d4feba08f1a9438ae788a7647cda6
Luis <luisfonsecac26@gmail.com>
2024-01-16 23:08:38 -0600

    useState Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,26 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const increment = (by = 1) => setCount(prevState => prevState += by)
+  const decrement = (by = 1) => setCount(prevState => prevState -= by)
+
+  const handleChange = (event) => setCount(Number(event.target.value)) 
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Counter</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button className='bg-green' onClick={() => decrement(10)}>-10</button>
+        <button className='bg-yellow' onClick={() => decrement()}>-1</button>
+
+        <input type="number" value={count} onChange={handleChange}/>
+        
+        <button className='bg-yellow' onClick={() => increment()}>+1</button>
+        <button className='bg-green' onClick={() => increment(10)}>+10</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
